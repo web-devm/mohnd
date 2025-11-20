8f32b4a28178ca9ebdbca0456a2a0327c500c6ce
web-devm <webdev.m2001@gmail.com>
2025-11-20 11:42:12 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here 
-  output: 'standalone', // <--- هذا السطر السحري
-  */
-};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    distDir: 'build',
+    output: 'standalone',
+    };
+  
+  
+  export default nextConfig;
