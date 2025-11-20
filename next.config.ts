e2e6323cee260553ace55bf1c4bb46111cab8cfe
web-devm <webdev.m2001@gmail.com>
2025-11-20 11:17:02 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone', // <--- هذا السطر السحري
 };
 
 export default nextConfig;
