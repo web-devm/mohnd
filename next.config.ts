71746ed2a373aba0291652e5c5548838ff19976f
web-devm <webdev.m2001@gmail.com>
2025-11-20 11:30:27 +0300

    Add standalone output option in next.config.ts
    Added output option to next.config.ts for standalone build.

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* config options here 
   output: 'standalone', // <--- هذا السطر السحري
+  */
 };
 
 export default nextConfig;
